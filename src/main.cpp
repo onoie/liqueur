dc216d939eef1051d292df362c98ff0dde50ab79
onoie <onoie3@gmail.com>
2017-07-29 07:35:43 +0900

    rename & reformat

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 int main(int argc, char *argv[]) {
     cout << "boost:v" << (BOOST_VERSION / 100000) << "." << (BOOST_VERSION / 100 % 1000) << "." << (BOOST_VERSION % 100) << endl;
     wcout << "date:" << formatDateWstr(boost::posix_time::second_clock::local_time()) << endl;
--- src/slib.cpp
+++ src/slib.cpp
@@ -7,6 +7,7 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
+
 void hello(){
     std::cout << "Hello, World!" << std::endl;
 }
