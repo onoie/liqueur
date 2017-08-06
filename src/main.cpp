52fe837dd88e5cb3d2e234ae140ca2dbfdc96338
onoie <onoie3@gmail.com>
2017-08-06 19:35:23 +0900

    fast commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <fstream>
 
-#include "boost/date_time/posix_time/posix_time.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/program_options.hpp>
 
 #include "slib.h"
